c77e0f4c6d7c6ad425f63906f933ae1b0578b0f1
Aaron Santos <asantosabsalon@gmail.com>
2025-03-14 09:03:04 -0500

    add: Nuevos componentes a la app
    Se añaden 4 nuevos componentes principales para
    el acceso a la app (login sign-up) y sus respectivas secciones
    (galeria y perfil). Ademas de su enrutado en app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'; // Componente home
 import { AppRoutingModule } from './app-routing.module';
 
+//! Importación de componentes Home, login, sign-up, galeria y perfil:
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { LoginComponent } from './components/login/login.component';
+import { GaleriaComponent } from './components/galeria/galeria.component';
+import { PerfilComponent } from './components/perfil/perfil.component'
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    SignUpComponent,
+    LoginComponent,
+    GaleriaComponent,
+    PerfilComponent,
+  ],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
