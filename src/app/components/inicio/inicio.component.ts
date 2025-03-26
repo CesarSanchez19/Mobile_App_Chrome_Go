2a27dcbdacf782c4bb8e42f4ed527d5761f4c96e
CesarSanchez19 <2302073@utrivieramaya.edu.mx>
2025-03-26 10:20:21 -0500

    add(home): agregar funcionalidad de usar la camarade la computadora

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -6,12 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inicio.component.scss'],
   standalone: false,
 })
-export class InicioComponent  implements OnInit {
+export class InicioComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {}
-
-  prueba = console.log("Inicio works! HOME funcionando correctamente !!!")
-
+  ngOnInit() {
+    // Solicita acceso a la cámara usando la API getUserMedia
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+        .then(stream => {
+          const video = document.getElementById('bg-video') as HTMLVideoElement;
+          if (video) {
+            video.srcObject = stream;
+            video.play();
+          }
+        })
+        .catch(err => {
+          console.error("Error al acceder a la cámara: ", err);
+        });
+    } else {
+      console.error("getUserMedia no es soportado en este navegador.");
+    }
+  }
 }
